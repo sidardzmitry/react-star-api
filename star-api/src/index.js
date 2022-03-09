51275b3a0edfad2dfd701e2464ea70ced8ad6aa9
Dmitry Sidortsov <dmx3214@mail.ru>
2022-03-09 19:16:46 +0300

    star
    star

--- star-api/src/index.js
+++ star-api/src/index.js
@@ -1,31 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-class SwapiService {
-  _apiBase = 'https://swapi.dev/api';
-  async getResource(url) {
-const response = await fetch(`${this._apiBase}${url}`);
-  if(!response.ok) {
-    throw new Error(`Could not fetch ${url}` +
-    `, received ${response.status}`);
-  };
-  return await response.json();
-  };
-
-  async getAllPeople() {
-    const response = await this.getResource(`/people/`)
-    return response.results;
-  }
-
-  getPerson(id) {
-    this.getPerson(`/people/${id}/`)
-  }
-};
-
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then((people) => {
-  people.forEach((p) => {
-    console.log(p.name)
-  })
-})
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
