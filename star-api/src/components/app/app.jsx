b221cd686effaeddb324e718d892db7095178078
Dmitry Sidortsov <dmx3214@mail.ru>
2022-03-16 17:30:02 +0300

    ктен

--- star-api/src/components/app/app.jsx
+++ star-api/src/components/app/app.jsx
@@ -1,47 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ErrorBoundry from '../error-boundry';
-import SwapiService from '../../services/swapi-service';
-import DummySwapiService from '../../services/dummy-swapi-service';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ErrorBoundry from "../error-boundry";
+import SwapiService from "../../services/swapi-service";
+import DummySwapiService from "../../services/dummy-swapi-service";
 
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
-import { SwapiServiceProvider } from '../swapi-service-context';
+import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages";
+import { SwapiServiceProvider } from "../swapi-service-context";
 
-import './app.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./app.css";
 export default class App extends Component {
-
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
   };
 
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
-      const Service = swapiService instanceof SwapiService ?
-                        DummySwapiService : SwapiService;
+      const Service =
+        swapiService instanceof SwapiService ? DummySwapiService : SwapiService;
       return {
-        swapiService: new Service()
+        swapiService: new Service(),
       };
     });
   };
 
   render() {
-
     return (
       <ErrorBoundry>
-        <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
+        <SwapiServiceProvider value={this.state.swapiService}>
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
+              
+              <PeoplePage />
+              <PlanetsPage />
+              <StarshipsPage />
 
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
-
-          </div>
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
-  };
-};
+  }
+}
